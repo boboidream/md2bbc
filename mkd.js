3684a12f135038a5a67042ac25b58a67558e850e
boboidream <z644381492@gmail.com>
2018-05-18 23:06:13 +0800

    [*] change h1 & h2 style

--- mkd.js
+++ mkd.js
@@ -11,26 +11,26 @@ var conv_opts = {
 
 var togglemultiline = function(){
 	conv_opts.multiline_quoting = !conv_opts.multiline_quoting;
-	console.log("(un)checked, now ", conv_opts);
+	// console.log("(un)checked, now ", conv_opts);
 }
 
 var togglequotesintolists = function(){
 	conv_opts.check_quotes_into_lists = !conv_opts.check_quotes_into_lists;
-	console.log("(un)checked, now ", conv_opts);
+	// console.log("(un)checked, now ", conv_opts);
 }
 
 var togglebbcode = function(){
 	conv_opts.recognize_bbcode = !conv_opts.recognize_bbcode;
-	console.log("(un)checked, now ", conv_opts);
+	// console.log("(un)checked, now ", conv_opts);
 }
 
 var toggleautolinking = function(){
 	conv_opts.enable_autolinking = !conv_opts.enable_autolinking;
-	console.log("(un)checked, now ", conv_opts);
+	// console.log("(un)checked, now ", conv_opts);
 }
 
 var shitconvert = function(){
-	console.log("converting with options: ", conv_opts);
+	// console.log("converting with options: ", conv_opts);
 	var converter = new Showdown.converter(conv_opts);	
 }
 
